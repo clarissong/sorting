cef4bc0916b37fa3a2b07224982a6f3586931c5a
clarissong <clarissadiandra@gmail.com>
2021-08-29 19:12:27 +0200

    A folder for all sorting algorithms written in Java along with a test Java class

---
+++ Java/src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * This is a test class. It only prints "Hello World!"
+ *
+ * @author clarissong
+ * @since 29.08.2021 19:07
+ */
+public class Test {
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
